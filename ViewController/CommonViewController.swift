923be1e675997da70367cd894d583c6e11d35704
David Thorn <thorn@adesso-mobile.de>
2020-04-10 14:29:09 +0200

    Added CommonViewController

---
+++ ViewController/CommonViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CommonViewController.swift
+//  DTKit
+//
+//  Created by Thorn, David on 10.04.20.
+//
+
+import UIKit
+
+open class CommonViewController<T: Hashable>: UIViewController {
+
+    public typealias ViewModel = T
+
+    public var viewModel: ViewModel?
+
+    public init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    open override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+}
