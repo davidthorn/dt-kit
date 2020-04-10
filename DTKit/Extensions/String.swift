8e84c54e313da1cab2466ea41d86bd5aa4d1982f
David Thorn <thorn@adesso-mobile.de>
2020-04-10 13:54:01 +0200

    develop: Added String extension

---
+++ DTKit/Extensions/String.swift
@@ -0,0 +1,18 @@
+//
+//  String.swift
+//  DTKit
+//
+//  Created by Thorn, David on 10.04.20.
+//
+
+import Foundation
+
+public extension String {
+
+    /// Returns a NSAttributedString created using this strings value and the attributes.
+    /// - Parameter attributes: The attributes that are used for the attributed string.
+    func with(attributes: [NSAttributedString.Key: Any]) -> NSAttributedString {
+        return .init(string: self, attributes: attributes)
+    }
+
+}
