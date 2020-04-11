5ae485eab10501953d7105d7b77455b648ed33f8
David Thorn <thorn@adesso-mobile.de>
2020-04-11 19:56:05 +0200

    Added CommonScrollView

---
+++ DTKit/Common/CommonScrollView.swift
@@ -0,0 +1,14 @@
+//
+//  CommonScrollView.swift
+//  DTKit
+//
+//  Created by Thorn, David on 11.04.20.
+//
+
+import UIKit
+
+open class CommonScrollView: UIScrollView {
+
+    public func commonInit() { }
+
+}
