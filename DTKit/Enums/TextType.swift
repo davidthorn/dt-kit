2eb00649c167ae61b449f72e9b353ab94817f2bf
David Thorn <thorn@adesso-mobile.de>
2020-04-11 10:18:09 +0200

    Added Enums folder to the project

---
+++ DTKit/Enums/TextType.swift
@@ -0,0 +1,19 @@
+//
+//  TextType.swift
+//  DTKit
+//
+//  Created by Thorn, David on 10.04.20.
+//
+
+import Foundation
+
+/// Enum used to define the type of text used to provide methods to handle more types.
+public enum TextType {
+    /// Case to handle using just an optional string
+    /// - Parameter value: The optional string that should be used.
+    case text(value: String?)
+
+    /// Case to handle using just an optional NSAttributedString
+    /// - Parameter text: The attributed string that should be used.
+    case attributed(text: NSAttributedString?)
+}
