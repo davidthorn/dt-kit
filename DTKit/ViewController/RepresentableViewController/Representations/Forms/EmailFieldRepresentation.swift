0ce6bece160fad998068cbfc5cf34f3291a4d8bd
David Thorn <thorn@adesso-mobile.de>
2020-04-11 20:06:12 +0200

    Added Form Field representations.

---
+++ DTKit/ViewController/RepresentableViewController/Representations/Forms/EmailFieldRepresentation.swift
@@ -0,0 +1,19 @@
+//
+//  EmailFieldRepresentation.swift
+//  DTKit
+//
+//  Created by Thorn, David on 11.04.20.
+//
+
+import Foundation
+
+public class EmailFieldRepresentation<T: InputFieldRepresentationType & Hashable>: InputFieldRepresentation<T> {
+
+    override public func createField() -> InputField<T> {
+        let field = InputField<T>()
+        field.keyboardType = .emailAddress
+        return field
+    }
+
+
+}
