77cbb7ccba44e2432c4a6382c7c16ad3e6612271
David Thorn <thorn@adesso-mobile.de>
2020-04-12 21:07:03 +0200

    Added textType property to NSAttributedString and String

--- DTKit/Core/Enums/TextType.swift
+++ DTKit/Core/Enums/TextType.swift
@@ -16,4 +16,33 @@ public enum TextType: Hashable {
     /// Case to handle using just an optional NSAttributedString
     /// - Parameter text: The attributed string that should be used.
     case attributed(text: NSAttributedString?)
+
+    var string: String? {
+        switch self {
+        case .attributed(let text):
+            return text?.string
+        case .text(let value):
+            return value
+        }
+    }
+
+    var identifier: String {
+        string ?? UUID().uuidString
+    }
+}
+
+public extension NSAttributedString {
+
+    var textType: TextType {
+        .attributed(text: self)
+    }
+
+}
+
+public extension String {
+
+    var textType: TextType {
+        .text(value: self)
+    }
+
 }
