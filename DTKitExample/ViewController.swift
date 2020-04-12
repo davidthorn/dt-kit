eb8fb096e20b9a241819628af734cd24648504a8
David Thorn <thorn@adesso-mobile.de>
2020-04-12 12:31:24 +0200

    Added CommonButton

--- DTKitExample/ViewController.swift
+++ DTKitExample/ViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import DTKit
 
 class ViewController: UIViewController {
 
+    let stackView: CommonStackView = .vertical
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .white
+
+        edgesForExtendedLayout = []
+
+        let commonButton: CommonButton = .common
+        commonButton.setTitle("Common", for: .normal)
+
+        let disabledButton: CommonButton = .disabled { button in
+            button.commonButtonType = .disabled
+        }
+        disabledButton.setTitle("Disabled", for: .normal)
+
+        let primaryButton: CommonButton = .primary { button in
+            disabledButton.commonButtonType = .secondary
+        }
+        primaryButton.setTitle("Primary", for: .normal)
+
+        let secondaryButton: CommonButton = .secondary { button in
+            disabledButton.commonButtonType = .disabled
+        }
+
+        secondaryButton.setTitle("Secondary", for: .normal)
+
+        view.addSubview(stackView)
+        stackView.pinTop(constant: 20)
+        stackView.pinBottom(lessThanOrEqualTo: 20)
+        stackView.pinHorizontal(insets: .horizontal(constant: 20))
+        stackView.spacing = 30
+
+        stackView.addArrangedSubview(commonButton)
+        stackView.addArrangedSubview(primaryButton)
+        stackView.addArrangedSubview(secondaryButton)
+        stackView.addArrangedSubview(disabledButton)
+
+    }
 
 }
 
