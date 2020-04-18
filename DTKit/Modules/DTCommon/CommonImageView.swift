75ba7efe4b41a662f72fd867f628bbfcdd414494
David Thorn <thorn@adesso-mobile.de>
2020-04-18 20:25:21 +0200

    Added unit tests for CommonImageView and added image assets.

--- DTKit/Modules/DTCommon/CommonImageView.swift
+++ DTKit/Modules/DTCommon/CommonImageView.swift
@@ -12,16 +12,21 @@ open class CommonImageView: UIImageView {
 
     public var bundleItem: BundleItem? {
         didSet {
-            image = bundleItem?.image?.withRenderingMode(.alwaysTemplate)
+            setImage(bundleItem: bundleItem)
         }
     }
 
-    public convenience init() {
-        self.init(image: nil)
+    // MARK: Constructors
+
+    public init() {
+        super.init(frame: .zero)
+        commonInit()
     }
 
-    public convenience init(bundleItem: BundleItem) {
-        self.init(image: bundleItem.image)
+    public init(bundleItem: BundleItem) {
+        self.bundleItem = bundleItem
+        super.init(frame: .zero)
+        commonInit()
     }
 
     public override init(frame: CGRect) {
@@ -35,17 +40,90 @@ open class CommonImageView: UIImageView {
     }
 
     required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        commonInit()
     }
 
-    open func commonInit() { }
+    // MARK: Common Init & Setup
+
+    open func commonInit() {
+        setImage(bundleItem: bundleItem)
+    }
+
+    // MARK: Private Helper Methods
+
+    /// Sets ImageViews image using the bundle item provided
+    /// - Parameter bundleItem: The bundle item containing the information about the image.
+    private func setImage(bundleItem: BundleItem?) {
+        guard bundleItem.isNotNil else { return }
+
+        image = bundleItem?.image?.withRenderingMode(.alwaysTemplate)
+    }
 
 }
 
+// MARK: Bundle Item - Image Extensions
+
 public extension BundleItem {
 
+    /// Returns an optional image using the CommonImageViews bundel Item property.
     var image: UIImage? {
         UIImage(named: imageName, in: bundle, compatibleWith: nil)
     }
 
 }
+
+// MARK: Static Helper Properties
+
+public extension UIImageView {
+
+    /// Returns an CommonImageView with the contentMode of scaleToFill
+    static var scaleToFill: CommonImageView = .scaleToFill(bundleItem: nil)
+
+    /// Returns an CommonImageView with the contentMode of scaleAspectFit
+    static var scaleAspectFit: CommonImageView = .scaleAspectFit(bundleItem: nil)
+
+    /// Returns an CommonImageView with the contentMode of scaleAspectFill
+    static var scaleAspectFill: CommonImageView = .scaleAspectFill(bundleItem: nil)
+
+}
+
+// MARK: Static Helper Methods
+
+public extension UIImageView {
+
+    /// Returns an CommonImageView with its image set and contentMode of scaleToFill
+    /// - Parameter bundleItem: The optional bundle item use to show the image
+    static func scaleToFill(bundleItem: BundleItem? = nil) -> CommonImageView {
+        contentMode(bundleItem: bundleItem, contentMode: .scaleToFill)
+    }
+
+    /// Returns an CommonImageView with its image set and contentMode of scaleAspectFit
+    /// - Parameter bundleItem: The optional bundle item use to show the image
+    static func scaleAspectFit(bundleItem: BundleItem? = nil) -> CommonImageView {
+        contentMode(bundleItem: bundleItem, contentMode: .scaleAspectFit)
+    }
+
+    /// Returns an CommonImageView with its image set and contentMode of scaleAspectFill
+    /// - Parameter bundleItem: The optional bundle item use to show the image
+    static func scaleAspectFill(bundleItem: BundleItem? = nil) -> CommonImageView {
+        contentMode(bundleItem: bundleItem, contentMode: .scaleAspectFill)
+    }
+
+    static func contentMode(
+        bundleItem: BundleItem? = nil,
+        contentMode: UIView.ContentMode,
+        tintColor: UIColor? = nil
+    ) -> CommonImageView {
+        let view: CommonImageView = .init()
+        if let tintColor = tintColor {
+            view.tintColor = tintColor
+        }
+        view.bundleItem = bundleItem
+        view.contentMode = contentMode
+
+        return view
+    }
+
+}
+
