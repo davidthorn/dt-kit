94b85715ddf5241d99062ce3e2ecbe5f64dddcbc
David Thorn <thorn@adesso-mobile.de>
2020-04-18 19:43:17 +0200

    Added documentation for CommonButtonType

--- DTKit/Modules/DTCommon/Buttons/CommonButtonType.swift
+++ DTKit/Modules/DTCommon/Buttons/CommonButtonType.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
+/// Determines how a CommonButton will be styled
 public enum CommonButtonType: Hashable {
+
+    /// Considered to be the main button that is used in all applications
     case primary
+
+    /// Considered to be the additional buttont that is not the primary button
     case secondary
-    case disabled
+
+    /// A button with a standard background and text color
     case common
+
+    /// A disabled common button
+    case disabled
+
+    /// A common that is used to display that this buttons action will perform a delete of some kind.
     case destructive
+
+    /// A custom button in the parameters determine the background color and title color.
     case custom(UIColor, UIColor)
+    
 }
